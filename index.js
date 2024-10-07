a762803b1b29bd215904b65dc54549ec7d2746b1
lauren-devon <laurendevonparker7@gmail.com>
2024-10-07 14:08:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import fs from "fs";
+import https from "https";
 import { notFound } from "./src/middlewares/notFound.js";
 import { handleError } from "./src/middlewares/handleError.js";
 import authRoute from "./src/api/auth/auth_route.js";
@@ -14,6 +16,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+const serverOptions = {
+  cert: fs.readFileSync('server-cert.pem'),
+  key: fs.readFileSync('server-key.pem')
+};
+
 //middleware
 app.use(express.json());
 
@@ -25,8 +32,22 @@ app.use("/api/button", buttonRoute);
 app.use(notFound);
 app.use(handleError);
 
-app.listen(port, () => {
-  console.log(`server running on port ${port}`);
+app.use((err, req, res, next) => {
+  console.error('Error occurred:', err.message);
+  console.error(err.stack); // Log stack trace
+  res.status(err.status || 500).json({
+    error: {
+      message: err.message,
+    },
+  });
+});
+
+// Create HTTPS server
+const httpsServer = https.createServer(serverOptions, app);
+
+// Start the server
+httpsServer.listen(port, () => {
+  console.log(`HTTPS server running on port ${port}`);
 });
 
 startButtonWebSocketServer();
