c796d8b92735af0658fcd29b46fff8323cb30663
lauren-devon <laurendevonparker7@gmail.com>
2024-10-07 13:33:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import { startMobileWebSocketServer } from "./src/websocket/mobile_socket.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(express.json());
